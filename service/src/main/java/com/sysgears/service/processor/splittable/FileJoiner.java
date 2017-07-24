74b814d17341541b3e897984d9ec5582c5ca9f1e
YuriHeiko <yuriheiko@gmail.com>
2017-07-24 16:58:16 +0300

    Added the Join command

--- service/src/main/java/com/sysgears/service/processor/splittable/FileJoiner.java
+++ service/src/main/java/com/sysgears/service/processor/splittable/FileJoiner.java
@@ -2,14 +2,19 @@ package com.sysgears.service.processor.splittable;
 
 import com.sysgears.service.ServiceException;
 import com.sysgears.service.processor.processable.FileChunk;
+import com.sysgears.service.processor.processable.IProcessable;
 
 import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
+import java.util.Iterator;
 
-public class FileJoiner {
+public class FileJoiner implements Iterator<IProcessable> {
     private final String fileName;
     private final String partPrefix;
-    private final RandomAccessFile destination;
+    private final RandomAccessFile source;
+    private final long fileSize;
+    private final long chunkSize;
+    private final Iterator<Long> iterator;
 
     private int partNumber;
 
@@ -18,21 +23,38 @@ public class FileJoiner {
                         final long chunkSize,
                         final String partPrefix,
                         final int partNumber) {
-        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.fileName = fileName.substring(0, fileName.lastIndexOf(partPrefix));
+        this.chunkSize = chunkSize;
         this.partPrefix = partPrefix;
         this.partNumber = partNumber;
+        this.iterator = new FileIterator(fileSize, chunkSize);
 
         try {
-            destination = new RandomAccessFile(fileName, "rw");
+            source = new RandomAccessFile(fileName, "rw");
         } catch (FileNotFoundException e) {
             throw new ServiceException(fileName + " doesn't exist.");
         }
-
-//        regress = fileSize;
     }
 
-    public FileChunk next() {
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
 
-        return null;
+    @Override
+    public FileChunk next() {
+        RandomAccessFile destination;
+
+        try {
+            destination = new RandomAccessFile(fileName + partPrefix + ++partNumber, "rw");
+        } catch (FileNotFoundException e) {
+            throw new ServiceException(fileName + partPrefix + ++partNumber + " wrong file name.");
+        }
+
+        long offset = iterator.next();
+        long size = fileSize - offset > chunkSize ? chunkSize : fileSize - offset;
+
+        return new FileChunk(destination, source, size, 0, offset);
     }
 }
--- ui/src/main/java/com/sysgears/ui/commands/CommandJoin.java
+++ ui/src/main/java/com/sysgears/ui/commands/CommandJoin.java
@@ -1,10 +1,24 @@
 package com.sysgears.ui.commands;
 
-import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.sysgears.io.IOHandler;
+import com.sysgears.io.SyncWriteIO;
+import com.sysgears.service.FileWorkerFactory;
+import com.sysgears.service.processor.IOProcessor;
+import com.sysgears.service.processor.IProcessableProcessor;
+import com.sysgears.service.processor.processable.IProcessable;
+import com.sysgears.service.processor.splittable.FileJoiner;
+import com.sysgears.statistic.ConcurrentRecordsHolder;
+import com.sysgears.statistic.IHolder;
+import com.sysgears.statistic.Watcher;
 import com.sysgears.ui.FileProcessor;
 import com.sysgears.ui.IExecutable;
+import com.sysgears.ui.ServiceRunner;
+import javafx.util.Pair;
+
+import java.io.File;
+import java.util.Iterator;
 
 /**
  * A command to glue a parts into one file
@@ -43,6 +57,30 @@ public class CommandJoin implements IExecutable {
      */
     @Override
     public void execute() {
-        System.out.println("join -p " + path);
+        final long chunkSize = new File(path).length();
+        final long fileSize = countFinalFileSize(path);
+        final IOHandler syncWriteIO = new SyncWriteIO();
+        final Iterator<IProcessable> fileJoiner = new FileJoiner(fileSize, path, chunkSize, partPrefix, 0);
+        final IHolder<Long, Pair<Long, Long>> holder = new ConcurrentRecordsHolder<>();
+        final Watcher<Long, Pair<Long, Long>> watcher = new Watcher<>(holder, fileSize, delay);
+        final IProcessableProcessor processor = new IOProcessor(syncWriteIO, holder, bufferSize);
+        final FileWorkerFactory fileWorkerFactory = new FileWorkerFactory(fileJoiner, processor);
+        final ServiceRunner serviceRunner = new ServiceRunner(fileWorkerFactory, watcher, threadsNumber);
+
+        serviceRunner.run();
+    }
+
+    long countFinalFileSize(final String fileName) {
+        int ind = fileName.lastIndexOf(partPrefix) + partPrefix.length();
+        final String name = fileName.substring(0, ind);
+        int number = Integer.parseInt(fileName.substring(ind));
+        long size = 0;
+
+        File file;
+        while ((file = new File(name + number++)).exists() && file.isFile()) {
+            size += file.length();
+        }
+
+        return size;
     }
 }
