91ae7a219e0e33690f62d5c66fa1c647ce248051
YuriHeiko <yuriheiko@gmail.com>
2017-07-26 18:26:15 +0300

    Added test-dependencies

---
+++ service/src/test/java/com/sysgears/service/processor/processable/factory/UTestFileChunkIterator.java
@@ -0,0 +1,21 @@
+package com.sysgears.service.processor.processable.factory;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class UTestFileChunkIterator {
+    @BeforeMethod
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testHasNext() throws Exception {
+    }
+
+    @Test
+    public void testNext() throws Exception {
+    }
+
+}
