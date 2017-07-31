ff32b3fa40a3c6aed512b09d1485a9c0cee3f0b8
YuriHeiko <yuriheiko@gmail.com>
2017-07-31 09:34:11 +0300

    Removed JIMFS dependency, added tests to UTestFileChunksSet

--- service/src/test/java/com/sysgears/service/processor/processable/factory/UTestFileChunksSet.java
+++ service/src/test/java/com/sysgears/service/processor/processable/factory/UTestFileChunksSet.java
@@ -1,20 +1,15 @@
 package com.sysgears.service.processor.processable.factory;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
 import com.sysgears.service.ServiceException;
 import com.sysgears.service.processor.processable.ChunkProperties;
 import com.sysgears.service.processor.processable.FileChunksSet;
 import org.easymock.EasyMockSupport;
 import org.testng.annotations.Test;
 
-import java.nio.file.FileSystem;
-
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-public class UTestFileChunkIterator extends EasyMockSupport {
-    private final FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
+public class UTestFileChunksSet extends EasyMockSupport {
 
     @Test(expectedExceptions = ServiceException.class)
     public void testHasNext1() throws Exception {
@@ -31,18 +26,24 @@ public class UTestFileChunkIterator extends EasyMockSupport {
         new FileChunksSet(-1, 0, 0, "", "").hasNext();
     }
 
-    @Test
-    public void testHasNext4() throws Exception {
-        assertTrue(new FileChunksSet(1, 1, 0, "", "").hasNext());
+    @Test(expectedExceptions = ServiceException.class)
+    public void testNext4() throws Exception {
+        FileChunksSet iterator = new FileChunksSet(0, 0, 0, "", "");
+        iterator.next();
     }
 
     @Test
     public void testHasNext5() throws Exception {
+        assertTrue(new FileChunksSet(1, 1, 0, "", "").hasNext());
+    }
+
+    @Test
+    public void testHasNext6() throws Exception {
         assertTrue(new FileChunksSet(2, 1, 0, "", "").hasNext());
     }
 
     @Test
-    public void testNext0() throws Exception {
+    public void testNext7() throws Exception {
         FileChunksSet iterator = new FileChunksSet(6, 2, 0, "a", ".b");
 
         assertTrue(iterator.hasNext());
